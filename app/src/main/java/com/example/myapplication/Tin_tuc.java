8e179fad5167020d8443c1fa7032c8d4896becf7
Ductrung611 <you@example.com>
2023-04-14 10:04:36 +0700

    commit

--- app/src/main/java/com/example/myapplication/Tin_tuc.java
+++ app/src/main/java/com/example/myapplication/Tin_tuc.java
@@ -18,41 +18,6 @@ public class Tin_tuc {
         this.link = link;
     }
 
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getDes() {
-        return des;
-    }
-
-    public void setDes(String des) {
-        this.des = des;
-    }
-
-    public String getPubDate() {
-        return pubDate;
-    }
-
-    public void setPubDate(String pubDate) {
-        this.pubDate = pubDate;
-    }
-
-    public String getLink() {
-        return link;
-    }
 
     public void setLink(String link) {
         this.link = link;
